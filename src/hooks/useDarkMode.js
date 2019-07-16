342a82bed4d98210ed0b146c0e5401349162f066
Zolton <gmzesq@gmail.com>
2019-07-16 17:23:48 -0400

    Stretch goal done - added Router and functioning buttons between pages

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 function useDarkMode() {
-
   const [darkMode, setDarkMode] = useLocalStorage("isDarkModeEnabled");
 
   useEffect(() => {
-    darkMode ? 
-    document.body.classList.add("dark-mode") : 
-    document.body.classList.remove("dark-mode");
+    darkMode
+      ? document.body.classList.add("dark-mode")
+      : document.body.classList.remove("dark-mode");
   }, [darkMode]);
 
   return [darkMode, setDarkMode];
