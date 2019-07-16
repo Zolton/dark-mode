4795d89cab70b9920e4177ccffd1bfb06b3746b0
Zolton <gmzesq@gmail.com>
2019-07-16 15:27:54 -0400

    WIP, working on useDarkMode comp

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,15 @@
-import React, {useState, useEffect} from "react"
-import useLocalStorage from "./useLocalStorage"
+import React, { useState, useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
 
-function useDarkMode () {
-    const [value, setValue] = useLocalStorage("darkModeEnabled")
-    useEffect( () => {
-        useLocalStorage ? {/* add darkmode*/} : {/* remove darkmode*/}
-    }, [])
+function useDarkMode() {
+  const [value, setValue] = useLocalStorage("isDarkModeEnabled");
+  const [darkMode, setDarkMode] = useState(false);
 
+  useEffect(() => {
+    useLocalStorage ? setDarkMode(true) : setDarkMode(false);
+  }, [darkMode]);
+
+  return [storedValue, setValue];
 }
 
-export default useDarkMode
+export default useDarkMode;
